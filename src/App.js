8fc9665e69af53cf2a5f8fba0745b89e5c46bba8
gnn06 <gilles.orsini@gmail.com>
2019-09-05 22:08:26 +0200

    store and restore albums

--- src/App.js
+++ src/App.js
@@ -143,13 +143,17 @@ class ImageList extends React.Component {
 }
 
 class Album extends React.Component {
+
   constructor () {
     super();
     this.state = {
       albums : []
     };
     this.request_albums = this.request_albums.bind(this);
+    this.store_albums = this.store_albums.bind(this);
+    this.restore_albums = this.restore_albums.bind(this);
   }
+
   request_albums() {
     var request = gapi.client.request({
       'method': 'GET',
@@ -162,10 +166,21 @@ class Album extends React.Component {
       component.setState({ albums: response.albums});
     });
   }
+
+  store_albums() {
+    localStorage.setItem('albums', JSON.stringify(this.state.albums));
+  }
+
+  restore_albums() {
+    var albums = JSON.parse(localStorage.getItem('albums'));
+    this.setState({albums: albums});
+  }
   
   render () {
     return <div>
       <button onClick={this.request_albums}>request albums</button>
+      <button onClick={this.store_albums}>store albums</button>
+      <button onClick={this.restore_albums}>restore albums</button>
       <div>{this.state.albums.map((item) => <span>{item.title}</span>)}</div>
       </div>;
   }
