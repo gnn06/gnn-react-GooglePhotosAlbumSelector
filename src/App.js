66dbc49c1710f2738f535a7e04cbdd99806565c3
gnn06 <gilles.orsini@gmail.com>
2019-09-08 21:54:56 +0200

    retrieve photo list of albums

--- src/App.js
+++ src/App.js
@@ -152,6 +152,7 @@ class Album extends React.Component {
     this.request_albums = this.request_albums.bind(this);
     this.store_albums = this.store_albums.bind(this);
     this.restore_albums = this.restore_albums.bind(this);
+    this.request_allAlbumPhotos = this.request_allAlbumPhotos.bind(this);
   }
 
   request_albums() {
@@ -167,6 +168,40 @@ class Album extends React.Component {
     });
   }
 
+  request_albumPhotos(album, nextPageToken) {
+    let component = this;
+    let params = { albumId : album.id };
+    if (nextPageToken != undefined) {
+      params.pageToken = nextPageToken;
+    }
+    let request = gapi.client.request({
+      'method': 'POST',
+      'path': 'https://photoslibrary.googleapis.com/v1/mediaItems:search',
+      params: params
+    });
+    request.execute(function (response) {
+      let albums = component.state.albums;
+      if (album.photos) {
+        album.photos = album.photos.concat(response.mediaItems);
+      } else {
+        album.photos = response.mediaItems;
+      }      
+      component.setState({albums : albums});
+      if (response.nextPageToken) {
+        component.request_albumPhotos(album, response.nextPageToken);
+      }
+    });
+  }
+
+  request_allAlbumPhotos() {
+    var component = this;
+    const albums = this.state.albums;
+    albums.forEach(album => {
+      this.request_albumPhotos(album);
+    });
+    
+  }
+
   store_albums() {
     localStorage.setItem('albums', JSON.stringify(this.state.albums));
   }
@@ -179,9 +214,10 @@ class Album extends React.Component {
   render () {
     return <div>
       <button onClick={this.request_albums}>request albums</button>
+      <button onClick={this.request_allAlbumPhotos}>request album phptos</button>
       <button onClick={this.store_albums}>store albums</button>
       <button onClick={this.restore_albums}>restore albums</button>
-      <div>{this.state.albums.map((item) => <span>{item.title}</span>)}</div>
+      <div>{this.state.albums.map((item) => <span>{item.title} ({item.photos != undefined ? item.photos.length : 0} / {item.mediaItemsCount})</span>)}</div>
       </div>;
   }
 }
