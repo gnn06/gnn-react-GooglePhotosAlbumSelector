5d935c21828a0a8e7415c2c008aabf363c56e97f
gnn06 <gilles.orsini@gmail.com>
2019-09-25 21:09:56 +0200

    junit : add test filterOneAlbum

--- src/album-utils.test.js
+++ src/album-utils.test.js
@@ -11,34 +11,44 @@ it('not filtered, other hidden album', () => {
     expect(filtered).toEqual(false);
 });
 
-it('not filtered, no album', () => {
+it('hasAllAlbumToHide, not filtered, no album', () => {
     const filtered = AlbumUtil.hasAllAlbumToHide([], ["album1"]);
     expect(filtered).toEqual(false);
 });
 
-it('not filtered, no hidden album', () => {
+it('hasAllAlbumToHide, not filtered, no hidden album', () => {
     const filtered = AlbumUtil.hasAllAlbumToHide([{id:"album1", title:"title1"}], []);
     expect(filtered).toEqual(false);
 });
 
-it('not filterd two empty album list', () => {
+it('hasAllAlbumToHide, not filterd two empty album list', () => {
     const filtered = AlbumUtil.hasAllAlbumToHide([], []);
     expect(filtered).toEqual(false);
 });
 
-it('not filtered because other album', () => {
+it('hasAllAlbumToHide, not filtered because other album', () => {
     const filtered = AlbumUtil.hasAllAlbumToHide([{id:"album1", title:"title1"}, {id:"album2", title:"title2"}], ["album1"]);
     expect(filtered).toEqual(false);
 });
 
-it('not filtered because other album bis', () => {
+it('hasAllAlbumToHide, not filtered because other album bis', () => {
     const filtered = AlbumUtil.hasAllAlbumToHide([{id:"album1", title:"title1"}, {id:"album2", title:"title2"}], ["album2"]);
     expect(filtered).toEqual(false);
 });
 
-it('filtered because of 2 hidden albums', () => {
+it('hasAllAlbumToHide, filtered because of 2 hidden albums', () => {
     const filtered = AlbumUtil.hasAllAlbumToHide([{id:"album1", title:"title1"}, {id:"album2", title:"title2"}], ["album1", "album2"]);
     expect(filtered).toEqual(true);
 });
 
+
+it('filterOneAlbum, filtered', () => {
+    const filtered = AlbumUtil.filterOneAlbum([{id:"photo1", albums:[{id:"album1"}]}], ["album1"]);
+    expect(filtered).toEqual([]);
+});
+
+it('filterOneAlbum, not filtered', () => {
+    const filtered = AlbumUtil.filterOneAlbum([{id:"photo1", albums:[{id:"album1"}]}], ["album2"]);
+    expect(filtered).toEqual([{id:"photo1", albums:[{id:"album1"}]}]);
+});
 
