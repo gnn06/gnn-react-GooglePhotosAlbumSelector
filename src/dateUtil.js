5bebba088d35013a552d1fe0b9ddca48b0807c49
gnn06 <gilles.orsini@gmail.com>
2019-11-10 22:21:42 +0100

    fix: unused var

--- src/dateUtil.js
+++ src/dateUtil.js
@@ -1,7 +1,6 @@
 import moment from 'moment';
 
 export function moveBackOneMonth(dateFilter) {
-    const currentMonth = moment(dateFilter.end).month();
     const result = {
         start: moment.utc(dateFilter.end).subtract(1, 'months').startOf("month").toDate(),
         end: moment.utc(dateFilter.end).subtract(1, 'months').endOf('month').startOf('day').toDate()
